a4a069451230af08cbc3d6b0ef4bc36d6763ed59
Carlos Alberto Baltazar Hinojosa <A840159943@my.uvm.edu.mx>
2024-08-01 12:26:20 -0600

    ELiminación de residuos de componentes que no se usarán

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import flet as ft
 from view.table_accounts import TableAccounts
-from view.options import add_button, edit_button, delete_button, searcher
+from view.appbar_actions import add_button, edit_button, delete_button, searcher
 
 def main(page: ft.Page):
 
+    page.theme_mode           =  ft.ThemeMode.LIGHT
+    page.vertical_alignment   = ft.MainAxisAlignment.START
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.vertical_alignment = ft.MainAxisAlignment.START
-    page.theme_mode =  ft.ThemeMode.LIGHT
-    page.theme = ft.Theme(color_scheme_seed='green')
+    page.theme                = ft.Theme(color_scheme_seed='green')
 
     page.appbar = ft.AppBar(
         title=searcher,
---
+++ view/appbar_actions.py
@@ -0,0 +1,24 @@
+import flet as ft
+
+searcher = ft.TextField(
+    prefix_icon=ft.icons.SEARCH,
+    label='Buscar usuario o cuenta',
+    border_radius=10,
+    width=400,
+    height=40,
+    text_size=13,
+    label_style=ft.TextStyle(size=13),
+)
+
+edit_button = ft.IconButton(
+    ft.icons.EDIT, icon_size=20,
+    tooltip='Editar',
+)
+add_button = ft.IconButton(
+    ft.icons.ADD, icon_size=20,
+    tooltip='Registrar',
+)
+delete_button = ft.IconButton(
+    ft.icons.DELETE, icon_size=20,
+    tooltip='Eliminar',
+)

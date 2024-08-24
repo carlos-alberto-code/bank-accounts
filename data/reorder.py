58ba7fb9ec85d4c0808bbc925cd17f69c75ad517
Carlos Alberto Baltazar Hinojosa <A840159943@my.uvm.edu.mx>
2024-08-24 11:38:20 -0600

    Se eliminó el archivo que reordeno el archivo csv

--- data/reorder.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('data/accounts.csv')
-df = df[['Nombres', 'Apellido Paterno', 'Apellido Materno', 'Numero de Cuenta']]
-df.to_csv('data/accounts.csv', index=False)
